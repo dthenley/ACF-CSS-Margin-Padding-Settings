3e2d74842fc4c7a2b0cbc1343e0f0523aba59445
Donte <3503886+dthenley@users.noreply.github.com>
2021-01-02 14:21:33 -0600

    Fixing Some Spacing

--- acf-css_margin_padding-v5.php
+++ acf-css_margin_padding-v5.php
@@ -67,17 +67,22 @@ class acf_field_css_margin_padding extends acf_field {
 		
 		
 		add_action( 'admin_enqueue_scripts', 'wptuts_add_color_picker' );
+
 		function wptuts_add_color_picker( $hook ) {
 		 
-		if( is_admin() ) {
-		 
-		        // Add the color picker css file
-		    wp_enqueue_style( 'wp-color-picker' );
-		 
-		        // Include our custom jQuery file with WordPress Color Picker dependency
-		        wp_enqueue_script( 'custom-script-handle', plugins_url( 'js/input.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
-		    }
+			if( is_admin() ) {
+			
+					// Add the color picker css file
+					wp_enqueue_style( 'wp-color-picker' );
+				}
+
 		}
+		//if( is_admin() ) {
+
+			// Include our custom jQuery file with WordPress Color Picker dependency
+			wp_enqueue_script( 'acf-css-margin-padding-js', plugins_url( 'js/input.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+		//}
+
 
 		// do not delete!
     	parent::__construct();
